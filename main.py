ee784e010d8c1034b6b6c30a3aed84ff27ff4b0d
Peter <sb1285n@gmail.com>
2024-03-02 18:27:33 -0500

    Fixed an error with starting up server

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from vector import Vector
 from vector_visualizer import VectorVisualizer
+import os
 
 vectors = [Vector(0, 0, 10, 10), Vector(0, 0, 20, 10)]
 visualizer = VectorVisualizer(vectors)
 server = visualizer.get_server()  # This is the Flask server object
 
 if __name__ == "__main__":
-    server.run_server(debug=True, port=(8050))
+    server.run_server(debug=True, port=(os.environ.get('PORT') or 8050))
